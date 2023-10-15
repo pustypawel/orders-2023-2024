39934b061bc12a70642c4b521c4b745c4cee8324
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2023-10-15 11:39:10 +0200

    design Product interface facade prepare for Order interface facade design

--- src/main/java/pl/edu/wszib/api/order/OrderApi.java
+++ src/main/java/pl/edu/wszib/api/order/OrderApi.java
@@ -3,27 +3,20 @@ package pl.edu.wszib.api.order;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.Objects;
 import java.util.Set;
 
-public class OrderApi {
-    private final String id;
-    private final Instant createdAt;
-    private final Instant updatedAt;
-    private final Set<OrderLineApi> lines;
-    private final BigDecimal amount;
-
+public record OrderApi(
+        String id,
+        Instant createdAt,
+        Instant updatedAt,
+        Set<OrderLineApi> lines,
+        BigDecimal amount) {
     public OrderApi(String id,
                     Instant createdAt,
                     Instant updatedAt,
                     Collection<OrderLineApi> lines,
                     BigDecimal amount) {
-        this.id = id;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.lines = Set.copyOf(lines);
-        this.amount = amount;
+        this(id, createdAt, updatedAt, Set.copyOf(lines), amount);
     }
 
     public OrderApi(String id,
@@ -32,49 +25,5 @@ public class OrderApi {
                     OrderLineApi[] lines,
                     BigDecimal amount) {
         this(id, createdAt, updatedAt, Set.of(lines), amount);
-    }
-
-    public String id() {
-        return id;
-    }
-
-    public Instant createdAt() {
-        return createdAt;
-    }
-
-    public Instant updatedAt() {
-        return updatedAt;
-    }
-
-    public Set<OrderLineApi> lines() {
-        return lines;
-    }
-
-    public BigDecimal amount() {
-        return amount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderApi orderApi = (OrderApi) o;
-        return Objects.equals(id, orderApi.id) && Objects.equals(createdAt, orderApi.createdAt) && Objects.equals(updatedAt, orderApi.updatedAt) && Objects.equals(lines, orderApi.lines) && Objects.equals(amount, orderApi.amount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, createdAt, updatedAt, lines, amount);
-    }
-
-    @Override
-    public String toString() {
-        return "OrderApi{" +
-                "id='" + id + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                ", lines=" + lines +
-                ", amount=" + amount +
-                '}';
     }
 }
---
+++ src/main/java/pl/edu/wszib/api/order/OrderFacadeApi.java
@@ -0,0 +1,11 @@
+package pl.edu.wszib.api.order;
+
+public interface OrderFacadeApi {
+    // create
+    // getById
+    // addProduct
+    // removeProduct
+    // changeQuantity
+    // increaseQuantity
+    // decreaseQuantity
+}
